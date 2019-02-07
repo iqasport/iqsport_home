f9cc8a4a0c3a3f012981a3433babaadc03a78cde
Alexis Moody <amoodyldes@gmail.com>
2019-02-07 10:17:54 -0500

    Adjusts yarn version to reflect dev environment

--- src/templates/post.js
+++ src/templates/post.js
@@ -11,54 +11,54 @@ export const BlogPostTemplate = ({
   title,
   date,
   author,
-}) => {
-  return (
-    <section className="section">
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <div dangerouslySetInnerHTML={{ __html: content }} />
-            <div style={{ marginTop: `4rem` }}>
-              <p>
-                {date} - posted by{' '}
-                <Link to={`/author/${author.slug}`}>{author.name}</Link>
-              </p>
-              {categories && categories.length ? (
-                <div>
-                  <h4>Categories</h4>
-                  <ul className="taglist">
-                    {categories.map(category => (
-                      <li key={`${category.slug}cat`}>
-                        <Link to={`/categories/${category.slug}/`}>
-                          {category.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : null}
-              {tags && tags.length ? (
-                <div>
-                  <h4>Tags</h4>
-                  <ul className="taglist">
-                    {tags.map(tag => (
-                      <li key={`${tag.slug}tag`}>
-                        <Link to={`/tags/${tag.slug}/`}>{tag.name}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : null}
-            </div>
+}) => (
+  <section className="section">
+    <div className="container content">
+      <div className="columns">
+        <div className="column is-10 is-offset-1">
+          <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+            {title}
+          </h1>
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+          <div style={{ marginTop: `4rem` }}>
+            <p>
+              {date}
+              - posted by
+              {' '}
+              <Link to={`/author/${author.slug}`}>{author.name}</Link>
+            </p>
+            {categories && categories.length ? (
+              <div>
+                <h4>Categories</h4>
+                <ul className="taglist">
+                  {categories.map(category => (
+                    <li key={`${category.slug}cat`}>
+                      <Link to={`/categories/${category.slug}/`}>
+                        {category.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+            {tags && tags.length ? (
+              <div>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map(tag => (
+                    <li key={`${tag.slug}tag`}>
+                      <Link to={`/tags/${tag.slug}/`}>{tag.name}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
-    </section>
-  )
-}
+    </div>
+  </section>
+)
 
 BlogPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
