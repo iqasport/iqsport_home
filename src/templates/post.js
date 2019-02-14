bdcbb1f6322afc86a760f92867da2ab7551b5796
Alexis Moody <amoodyldes@gmail.com>
2019-02-14 17:37:19 -0500

    Removes unstyled tag and category list

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 
 const BlogPostTemplate = (props) => {
-  const { content, categories, tags, title, date, author } = props
+  const { content, title, date, author } = props
   return (
     <div className="container content">
       <section className="section has-background-white">
@@ -20,34 +20,8 @@ const BlogPostTemplate = (props) => {
                 {date}
                 - posted by
                 {' '}
-                <Link to={`/author/${author.slug}`}>{author.name}</Link>
+                {author.name}
               </p>
-              {categories && categories.length ? (
-                <div>
-                  <h4>Categories</h4>
-                  <ul className="taglist">
-                    {categories.map(category => (
-                      <li key={`${category.slug}cat`}>
-                        <Link to={`/categories/${category.slug}/`}>
-                          {category.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : null}
-              {tags && tags.length ? (
-                <div>
-                  <h4>Tags</h4>
-                  <ul className="taglist">
-                    {tags.map(tag => (
-                      <li key={`${tag.slug}tag`}>
-                        <Link to={`/tags/${tag.slug}/`}>{tag.name}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : null}
             </div>
           </div>
         </div>
@@ -94,14 +68,6 @@ export const pageQuery = graphql`
       slug
       content
       date(formatString: "MMMM DD, YYYY")
-      categories {
-        name
-        slug
-      }
-      tags {
-        name
-        slug
-      }
       author {
         name
         slug
