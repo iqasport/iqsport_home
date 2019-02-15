0a2a7b43360956e562828c3dbf7ee5b522ab1602
Alexis Moody <amoodyldes@gmail.com>
2019-02-14 23:18:35 -0500

    Removes author build

--- gatsby-node.js
+++ gatsby-node.js
@@ -172,38 +172,38 @@ exports.createPages = ({ actions, graphql }) => {
         })
       })
     })
-    .then(() => {
-      return graphql(`
-        {
-          allWordpressWpUsers {
-            edges {
-              node {
-                id
-                slug
-              }
-            }
-          }
-        }
-      `)
-    })
-    .then(result => {
-      if (result.errors) {
-        result.errors.forEach(e => console.error(e.toString()))
-        return Promise.reject(result.errors)
-      }
+    // .then(() => {
+    //   return graphql(`
+    //     {
+    //       allWordpressWpUsers {
+    //         edges {
+    //           node {
+    //             id
+    //             slug
+    //           }
+    //         }
+    //       }
+    //     }
+    //   `)
+    // })
+    // .then(result => {
+    //   if (result.errors) {
+    //     result.errors.forEach(e => console.error(e.toString()))
+    //     return Promise.reject(result.errors)
+    //   }
 
-      const authorTemplate = path.resolve(`./src/templates/author.js`)
+    //   const authorTemplate = path.resolve(`./src/templates/author.js`)
 
-      _.each(result.data.allWordpressWpUsers.edges, ({ node: author }) => {
-        createPage({
-          path: `/author/${author.slug}`,
-          component: authorTemplate,
-          context: {
-            id: author.id,
-          },
-        })
-      })
-    })
+    //   _.each(result.data.allWordpressWpUsers.edges, ({ node: author }) => {
+    //     createPage({
+    //       path: `/author/${author.slug}`,
+    //       component: authorTemplate,
+    //       context: {
+    //         id: author.id,
+    //       },
+    //     })
+    //   })
+    // })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
